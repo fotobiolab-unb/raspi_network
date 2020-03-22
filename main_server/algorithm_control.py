de588c7e8498f4981121753f9e49509d5f396fe7
Icaro Lorran <icarosadero@gmail.com>
2020-03-22 18:32:36 -0300

    Added more constraint options in algorithm_control.py

--- main_server/algorithm_control.py
+++ main_server/algorithm_control.py
@@ -17,11 +17,24 @@ import logging
 logging.basicConfig(filename="ga.log", level=logging.DEBUG)
 shared_memory = memcache.Client(['localhost'])
 
-def fitness(X_cube):
-        high = np.ones(X_cube.shape[1])
-        low = np.zeros(X_cube.shape[1])
+def fitness(X_cube, mode="volume", high=None, low=None, E=1):
+        """
+        mode: str
+            -volume: expects the energy constranint to be smaller than a threshold
+            -surface: expects the energy constraint to be equal to a threshold
+        """
+        high = np.ones(X_cube.shape[1]) if not high else high
+        low = np.zeros(X_cube.shape[1]) if not low else low
         scale = np.array([high,low])
-        X = hypercube_to_simplex(X_cube.copy(),scale)
+        if mode=="volume":
+            X = hypercube_to_simplex(X_cube.copy(),scale)
+        elif mode=="surface":
+            #In this case there is a dimension reduction
+            X = hypercube_to_simplex(X_cube.copy(),scale)
+            u = E - X.sum(axis=1)
+            u = u.reshape((u.shape[0], 1))
+            X = np.hstack([u,X])
+        print(X.sum(axis=1))
         print("X", X)
         shared_memory.set('batch_done', False)
         batch = hanashi.create_new_batch(X)
@@ -33,7 +46,7 @@ def fitness(X_cube):
 
 mask = np.array([[0.,1.] for i in range(4)])
 #mask = np.array([[-10.,10.] for i in range(4)])
-def evolve(*args, **kwargs):
+def evolve(mode, *args, **kwargs):
     ga = GA(
             population_size=8,
             chromosome_size=4,
@@ -41,7 +54,7 @@ def evolve(*args, **kwargs):
             iterations=10,
             elitism=True,
             mutation=0.2,
-            fitness=fitness,
+            fitness=lambda x: fitness(x, mode),
             range_mask=mask,
             has_mask=True,
             time_print=.5,
@@ -53,4 +66,4 @@ def evolve(*args, **kwargs):
 if __name__=="__main__":
     # X = np.random.random_sample((4,3))
     # print(fitness(X))
-    evolve()
+    evolve(mode="surface")
