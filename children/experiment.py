Fixed bug where fitness array was shuffled in the wrong order with respect to input X in algorithm_control.py. Fized bug where chromossome plot was off-scale on the main server home.

children/experiment.py
@@ -8,4 +8,5 @@ Wrapper for function used on the raspberries. Called on hanashi/static_set
 """
 
 def f(X):
-    return random.random()
+    X = np.array(X)
+    return 10*len(X)+(X*X-10*np.cos(2*np.pi*X)).sum()

main_server/algorithm_control.py
@@ -16,6 +16,7 @@ from waiting import wait
 import logging
 logging.basicConfig(filename="ga.log", level=logging.DEBUG)
 shared_memory = memcache.Client(['localhost'])
+np.set_printoptions(precision=3)
 
 def fitness(X_cube, mode="volume", high=None, low=None, E=1):
         """
@@ -34,18 +35,18 @@ def fitness(X_cube, mode="volume", high=None, low=None, E=1):
             u = E - X.sum(axis=1)
             u = u.reshape((u.shape[0], 1))
             X = np.hstack([u,X])
-        print("X", X)
+        print(X)
         shared_memory.set('batch_done', False)
         batch = hanashi.create_new_batch(X)
-        hanashi.step()
+        R = hanashi.step()
         wait(lambda : shared_memory.get('batch_done'))
         id = batch["id"]
-        y = np.matrix(hanashi.get_from_id(id))
-        print("Y", y)
-        print("id", id)
+        Y = hanashi.get_from_id(id, return_request_id=True)
+        Y = {u[1]:u[0] for u in Y}
+        y = [Y[k] for k in np.array(R)[:,2].astype(int)]
         return np.array(y).ravel()
 
-mask = np.array([[0.,1.] for i in range(4)])
+mask = np.array([[0,1.] for i in range(4)])
 #mask = np.array([[-10.,10.] for i in range(4)])
 def evolve(mode, *args, **kwargs):
     ga = GA(
@@ -59,7 +60,8 @@ def evolve(mode, *args, **kwargs):
             range_mask=mask,
             has_mask=True,
             time_print=.5,
-            logging=False
+            logging=True,
+            maximize=True
             )
     ga.G[0]=np.zeros(ga.G[0].shape)
     ga.run()
@@ -67,4 +69,4 @@ def evolve(mode, *args, **kwargs):
 if __name__=="__main__":
     # X = np.random.random_sample((4,3))
     # print(fitness(X))
-    evolve(mode="surface")
+    evolve(mode="volume")

main_server/hanashi.py
@@ -31,11 +31,11 @@ def get_available_servers():
     online = list(filter(lambda x: online[x].ok,online.keys()))
     return database_manager.fetch_children_subset(online)
 
-def get_from_id(id):
+def get_from_id(id,**kwargs):
     """
     Returns processed fitness in ascending order
     """
-    return database_manager.fitness_from_batch_id(id)
+    return database_manager.fitness_from_batch_id(id, **kwargs)
 
 def create_new_batch(X):
     """
@@ -86,7 +86,7 @@ def step():
     success = list(filter(lambda x: x!=None and x.ok,gmap))
     notification = f"Executed batch post, {len(success)} out of {len(requests)} returned 200."
     logging.info(notification)
-    return notification
+    return unresolved
 
 def get_best_genome_data(n=3):
     """
@@ -94,8 +94,12 @@ def get_best_genome_data(n=3):
     """
     genome_graph_names = database_manager.get_best_individual(n)
     raw = np.array([database_manager.get_genome_graph(x) for x in genome_graph_names])
-    genome_graph = np.array([[raw[j,:,:i].sum(axis=1) for i in range(1,len(raw[0][0])+1)] for j in range(len(raw))]).tolist()
-    print(np.array(genome_graph))
+    genome_graph = np.array([[raw[j,:,:i].sum(axis=1) for i in range(1,len(raw[0][0])+1)] for j in range(len(raw))])
+    # maximum, minimum = genome_graph.max(axis=1), genome_graph.min(axis=1)
+    # maximum = np.repeat(maximum[:,np.newaxis,:],genome_graph.shape[1],axis=1)
+    # minimum = np.repeat(minimum[:,np.newaxis,:],genome_graph.shape[1],axis=1)
+    # genome_graph = (genome_graph-minimum)/(maximum-minimum) if (maximum-minimum).sum()!=0 else genome_graph
+    genome_graph = genome_graph.tolist()
     genome_graph_names = ["R"+str(x) for x in genome_graph_names]
     return genome_graph_names, genome_graph
 