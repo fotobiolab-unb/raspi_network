2fb61ef1b4e32d679f5ca9c9b801197edbd179fa
Icaro <icarosadero@gmail.com>
2020-11-27 08:18:57 -0300

    Creater generator sets for the LED's.

---
+++ main_server/.ipynb_checkpoints/algorithm_control-checkpoint.py
@@ -0,0 +1,86 @@
+"""
+Path for gapy module must be specified in the config.json file
+gapy: https://github.com/icarosadero/gapy
+"""
+
+import sys
+import json
+import hanashi
+config = json.load(open("config.json"))
+sys.path.append(config["gapy_path"])
+from gapy import GA
+from gapy2 import GA as GA2
+from tools import hypercube_to_simplex
+import numpy as np
+import memcache
+from waiting import wait
+import logging
+logging.basicConfig(filename="ga.log", level=logging.DEBUG)
+shared_memory = memcache.Client(['localhost'])
+np.set_printoptions(precision=3)
+
+def fitness(X_cube, mode="volume", high=None, low=None, E=1):
+        """
+        mode: str
+            -volume: expects the energy constranint to be smaller than a threshold
+            -surface: expects the energy constraint to be equal to a threshold
+        """
+        high = np.ones(X_cube.shape[1]) if not high else high
+        low = np.zeros(X_cube.shape[1]) if not low else low
+        scale = np.array([high,low])
+        if mode=="volume":
+            X = hypercube_to_simplex(X_cube.copy(),scale)
+        elif mode=="surface":
+            #In this case there is a dimension reduction
+            X = hypercube_to_simplex(X_cube.copy(),scale)
+            u = E - X.sum(axis=1)
+            u = u.reshape((u.shape[0], 1))
+            X = np.hstack([u,X])
+        print(X)
+        shared_memory.set('batch_done', False)
+        batch = hanashi.create_new_batch(X)
+        R = hanashi.step()
+        wait(lambda : shared_memory.get('batch_done'))
+        id = batch["id"]
+        Y = hanashi.get_from_id(id, return_request_id=True)
+        Y = {u[1]:u[0] for u in Y}
+        y = [Y[k] for k in np.array(R)[:,2].astype(int)]
+        return np.array(y).ravel()
+
+mask = np.array([[0,1.] for i in range(4)])
+#mask = np.array([[-10.,10.] for i in range(4)])
+def evolve(mode, *args, **kwargs):
+    ga = GA(
+            population_size=8,
+            chromosome_size=4,
+            resolution=8,
+            iterations=10,
+            elitism=True,
+            mutation=0.2,
+            fitness=lambda x: fitness(x, mode),
+            range_mask=mask,
+            has_mask=True,
+            time_print=.5,
+            logging=True,
+            maximize=True
+            )
+    ga.G[0]=np.zeros(ga.G[0].shape)
+    ga.run()
+
+def evolve2(mode, *args, **kwargs):
+    ga = GA2(
+        population_size = 8,
+        mutation_probability = 0.2,
+        generations = 10,
+        resolution = 8,
+        ranges = mask,
+        elitism = 1
+    )
+    ga.f = lambda x: fitness(x, mode)
+    ga.run()
+
+if __name__=="__main__":
+    # X = np.random.random_sample((4,3))
+    # print(fitness(X))
+    #evolve(mode="surface")
+    evolve2(mode="surface")
---
+++ main_server/.ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,98 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+
+from flask import Flask, render_template, request, jsonify, send_from_directory, url_for
+import logging
+# import listener
+from logging import Formatter, FileHandler
+logging.basicConfig(filename="server.log", level=logging.DEBUG)
+import os
+import json
+import database_manager
+import hanashi
+import numpy as np
+import time
+from gevent.pywsgi import WSGIServer
+import memcache
+import time
+
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+app.debug = True
+
+config = json.load(open('config.json'))
+
+#----------------------------------------------------------------------------#
+# Controllers.
+#----------------------------------------------------------------------------#
+
+shared_memory = memcache.Client(['localhost'])
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    """
+    GET: Loads a homepage to control children raspberries.
+    POST: Receives info from children.
+    """
+    if request.method == "GET":
+        database_manager.get_exsiting_batch()
+        #data = hanashi.get_home_data()#
+        data = database_manager.get_home_data()
+        online = hanashi.get_available_servers()
+        genome_names, genome_graph = hanashi.get_best_genome_data()
+        return render_template('pages/home.html', children=list(online), graph=data["graph"], genome_name=genome_names, genome_graph=genome_graph)
+    elif request.method == "POST":
+        data = request.json
+        database_manager.update_assignment(fitness=data["fitness"], request_id=data["request_id"])
+        r = database_manager.get_exsiting_batch()
+        if len(r)==0:
+            #Updating status on cached memory
+            set_response = shared_memory.set('batch_done', True)
+            if set_response==0:
+                logging.warn('set_response error')
+        return "ok", 200
+
+@app.route('/assignments', methods=['GET', 'POST'])
+def assignments():
+    if request.method == "GET":
+        database_manager.get_exsiting_batch()
+        assignments = database_manager.fetch_assignments()
+        return render_template('pages/assignments.html', assignments=assignments)
+    elif request.method == "POST":
+        """
+        Manually add new data.
+        """
+        data = request.json["matrix"] #list of lists
+        hanashi.create_new_batch(np.array(data))
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+@app.route('/push', methods=['GET', 'POST'])
+def push():
+    """
+    Pushes all assignments to the children servers.
+    """
+    if request.method == "GET":
+        notification = hanashi.step()
+        return notification, 200
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
