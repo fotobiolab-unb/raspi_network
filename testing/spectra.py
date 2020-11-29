9c65fbe9d0c753e7dd61c706a5616d2f17f4a971
Icaro <icarosadero@gmail.com>
2020-11-29 13:27:14 -0300

    Added functions to set up spectra in raspberry pi on testing

--- testing/spectra.py
+++ testing/spectra.py
@@ -3,6 +3,7 @@ sys.path.append("../main_server/")
 import numpy as np
 import json
 import hanashi
+import serial
 
 def uniform(alpha):
     with open("../data/spectra/neutral_spectrum_components.json") as f:
@@ -11,3 +12,22 @@ def uniform(alpha):
         coefficients = alpha*coefficients
         hanashi.create_new_batch(coefficients.reshape((1,coefficients.shape[0])))
         hanashi.step()
+
+def init(*params):
+    s = serial.Serial(*params)
+    s.write("silent_connect\r\n".encode("ascii"))
+    return s
+
+def brilho(s,b):
+    s.write(f"set(brilho,{})\r\n".encode("ascii"))
+
+def set_uniform(s):
+    with open("../data/spectra/parameters.json") as f:
+        param = json.load(f)
+        param = sorted(param.items(), key= lambda x: x[0])
+        
+        for p,x in zip(param, X):
+            string = f"set({p[0]},{int(100*x)})\r\n" 
+            time.sleep(1)
+            print(string)
+            s.write(string.encode("ascii"))
