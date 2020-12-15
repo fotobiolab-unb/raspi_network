5d690ce0c0ef1af63c6019fb35b5cbe6394e2d2b
´Ícaro Lorran Lopes Costa <icarosadero@gmail.com>
2020-12-15 18:33:23 -0300

    .

--- testing/spectra.py
+++ testing/spectra.py
@@ -14,7 +14,7 @@ def uniform(alpha):
         hanashi.create_new_batch(coefficients.reshape((1,coefficients.shape[0])))
         hanashi.step()
 
-def shadow_uniform(alpha,address,time)
+def shadow_uniform(alpha,address,time):
     with open("../data/spectra/neutral_spectrum_components.json") as f:
         coefficients = json.load(f)["coefficients"]
         coefficients = np.array(coefficients)
